c9577bbb4eb4e109a80ead454dcc0c8fd9b2842e
Ivan <danilov@qtim.ru>
2024-11-17 12:38:59 +0500

    feat(header): сформировал палитру для primary цвета

--- src/pages/main-page/main-page.vue
+++ src/pages/main-page/main-page.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-
+import Container from '~/src/shared/ui/Container.vue'
 </script>
 
 <template>
-  <h1>Главная страница</h1>
+  <Container class="mt-6">
+    <h1>Главная страница</h1>
+  </Container>
 </template>
