e86e473517468aae42f917d903176b934b512468
Ivan <danilov@qtim.ru>
2024-11-19 18:21:21 +0500

    feat(sneakers-pagination): добавил пагинацию для кроссовок

--- src/entities/sneaker/model/store/sneakers-store.ts
+++ src/entities/sneaker/model/store/sneakers-store.ts
@@ -2,8 +2,11 @@ import { useAsyncData } from '#app'
 import { sneakersApi } from '~/src/shared/api/sneakers'
 import type { SneakerMinDto } from '~/src/shared/api/sneakers/types'
 import { useRouteQueryWatcher } from '~/src/shared/lib/composables/use-route-query-watcher'
+import { usePagination } from '~/src/shared/lib/composables/use-pagination'
 
 export const useSneakersStore = defineStore('sneakers', () => {
+  const { getPagePaginationQuery, currentPage, total, setTotal } = usePagination()
+
   const { customRoute, routeQuery, stopWatchRouter, startWatchRouter } = useRouteQueryWatcher()
 
   const { data: sneakersData, status, refresh } = useAsyncData('sneakers-data', fetchSneakers, {
@@ -18,15 +21,24 @@ export const useSneakersStore = defineStore('sneakers', () => {
 
   const pending = computed(() => status.value === 'pending')
   async function fetchSneakers () {
-    const { data } = await sneakersApi.getList(routeQuery.value)
+    const { data } = await sneakersApi.getList({
+      ...routeQuery.value,
+      ...getPagePaginationQuery()
+    })
+
+    setTotal(data.pagination.totalItems)
 
     return data.items
   }
+
+  watch(currentPage, async () => await refresh())
 
   return {
     sneakersData,
     pending,
     stopWatchRouter,
-    startWatchRouter
+    startWatchRouter,
+    total,
+    currentPage
   }
 })
--- src/pages/main-page/ui/main-page.vue
+++ src/pages/main-page/ui/main-page.vue
@@ -2,6 +2,10 @@
 import Container from '~/src/shared/ui/Container.vue'
 import SneakersList from '~/src/pages/main-page/ui/SneakersSection/SneakersList.vue'
 import SneakersFilter from '~/src/features/sneakers/sneakers-filter/ui/SneakersFilter.vue'
+import { useSneakersStore } from '~/src/entities/sneaker/model/store/sneakers-store'
+
+const store = useSneakersStore()
+const { currentPage, total } = storeToRefs(store)
 </script>
 
 <template>
@@ -10,7 +14,11 @@ import SneakersFilter from '~/src/features/sneakers/sneakers-filter/ui/SneakersF
       Каталог кроссовок
     </h1>
     <div class="grid grid-cols-[1fr_320px] gap-6 mt-6">
-      <SneakersList />
+      <div class="flex flex-col gap-4">
+        <SneakersList />
+        <UPagination v-model="currentPage" :total="total" />
+      </div>
+
       <SneakersFilter class="w-full" />
     </div>
   </Container>
